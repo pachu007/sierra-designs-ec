e3063baf63774ef3e152b7d537413f37554b0ece
pachu007 <79619570+pachu007@users.noreply.github.com>
2026-07-11 18:45:29 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/sierra-designs-ec/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
